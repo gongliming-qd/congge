b0fc37beb7cc48eaa3417ad0e3488450492c639f
gongliming <gongliming@tenorshare.cn>
2022-03-11 18:34:06 +0800

    图片

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import './index.less';
 
 export default function index() {
-  return <div>home</div>;
+  const clickImport = () => {};
+  return (
+    <div className="appendNew">
+      <div
+        className="importBtn"
+        onClick={() => {
+          clickImport();
+        }}
+      >
+        <img src={require('@/assets/imgs/append_import.svg')} />
+        <div className="importIntro">
+          <span className="topTntro">Import file</span>
+          <span className="bottomIntro">PDbea</span>
+        </div>
+      </div>
+      <div
+        className="appendBtn"
+        onClick={() => {
+          clickImport();
+        }}
+      >
+        <img src={require('@/assets/imgs/append_add.png')} />
+        <div className="importIntro">
+          <span className="topTntro">Add New</span>
+          <span className="bottomIntro">Here we go now!</span>
+        </div>
+      </div>
+    </div>
+  );
 }
